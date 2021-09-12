0c08a4e6f1a76d8cb78e805e3531104f956add1d
KarinaStabile <karina.stabile@gmail.com>
2021-09-12 12:14:30 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  //use reduce method to total the pages
+  //number 5 book
+  //1031st character
+  //fetch request to url, return jSON
+  //second function renderBooks pass in JSON as arg
+  //returns fetch
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(function(response) {
+      return response.json(); 
+    })
+    .then(renderBooks); 
 }
 
 function renderBooks(books) {
